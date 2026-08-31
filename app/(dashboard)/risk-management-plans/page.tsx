52e5eea11db6511bdcecf76c2952c1648fe19210
clintjeff2 <clintjeff2@gmail.com>
2026-08-31 22:38:25 +0100

    Add app/(dashboard)/risk-management-plans/page.tsx

---
+++ app/(dashboard)/risk-management-plans/page.tsx
@@ -0,0 +1,5 @@
+import { RiskManagementPlansScreen } from '@/components/screens/risk-management-plans'
+
+export default function RiskManagementPlansPage() {
+  return <RiskManagementPlansScreen />
+}

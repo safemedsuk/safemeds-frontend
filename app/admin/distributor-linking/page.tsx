a486be3091c45185a1644048c2a30880c06ceed1
clintjeff2 <clintjeff2@gmail.com>
2026-08-31 22:38:25 +0100

    Add app/admin/distributor-linking/page.tsx

---
+++ app/admin/distributor-linking/page.tsx
@@ -0,0 +1,5 @@
+import { DistributorLinkingAdmin } from '@/components/screens/distributor-linking-admin'
+
+export default function AdminDistributorLinkingPage() {
+  return <DistributorLinkingAdmin />
+}

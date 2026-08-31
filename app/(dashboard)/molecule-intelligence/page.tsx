0dc64e508c9bd4d9f80bbe80acf9ba969cb19a03
clintjeff2 <clintjeff2@gmail.com>
2026-08-31 22:38:25 +0100

    Add app/(dashboard)/molecule-intelligence/page.tsx

---
+++ app/(dashboard)/molecule-intelligence/page.tsx
@@ -0,0 +1,5 @@
+import { MoleculeIntelligenceScreen } from '@/components/screens/molecule-intelligence'
+
+export default function MoleculeIntelligencePage() {
+  return <MoleculeIntelligenceScreen />
+}

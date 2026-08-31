4858778aff6745ad6421144d1b33bea73c2a83cf
clintjeff2 <clintjeff2@gmail.com>
2026-08-31 22:38:25 +0100

    Add app/(dashboard)/literature-monitoring/page.tsx

---
+++ app/(dashboard)/literature-monitoring/page.tsx
@@ -0,0 +1,5 @@
+import { LiteratureMonitoringScreen } from '@/components/screens/literature-monitoring'
+
+export default function LiteratureMonitoringPage() {
+  return <LiteratureMonitoringScreen />
+}
